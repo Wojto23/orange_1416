e37844906d3f5aa74dcc9bd6dfcefdf54441b330
dev-com2020 <kwp@tlen.pl>
2023-06-14 15:16:04 +0200

    14.06 15:16

--- run.py
+++ run.py
@@ -6,18 +6,18 @@ from util.bike import Bike
 # Connect to API
 # network.connect_to_api()
 
-# rower1 = Bike("red", "BMX", represents=True)
-# rower2 = Bike("blue", "BMX", represents=False)
-#
-# rower1.brake()
-# rower1.model = "Romet"
-# print(rower1)
-# print(rower2)
-# lista = []
-# lista.append(rower1.model)
-# print(lista)
-# print(Bike.ilosc_egzemplarzy)
-# print(rower1.ilosc_egzemplarzy)
-# print(id(rower1))
-# print(id(rower2))
+rower1 = Bike("red", "BMX", represents=True)
+rower2 = Bike("blue", "BMX", represents=False)
+rower1.brake()
+rower1.model = "Romet"
+print(rower1)
+print(rower2)
+lista = []
+lista.append(rower1)
+lista.append(rower2)
+print(lista)
+print(Bike.ilosc_egzemplarzy)
+print(rower1.ilosc_egzemplarzy)
+print(id(rower1))
+print(id(rower2))
 
--- util/bike.py
+++ util/bike.py
@@ -1,16 +1,18 @@
 class Bike:
-    represents = True
     ilosc_egzemplarzy = 0
 
     def __init__(self, colour: str, model: str, represents):
         self.colour = colour
         self.model = model
-        Bike.represents = represents
+        self.represents = represents
         Bike.ilosc_egzemplarzy += 1
 
     def brake(self):
         print("Braking")
 
-    if represents:
-        def __repr__(self):
+    def __repr__(self):
+        if self.represents:
             return f"Rower {self.model} w kolorze {self.colour}"
+        else:
+            return super().__repr__()
+

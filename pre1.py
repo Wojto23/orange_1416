882e8235c5e2c750d75bea92c69ad07a11a7bc07
dev-com2020 <kwp@tlen.pl>
2023-06-15 15:45:01 +0200

    15.06 15:44

---
+++ pre1.py
@@ -0,0 +1,25 @@
+import argparse
+import configparser
+import sys
+
+
+def main(number, other_number, output):
+    result = number + other_number
+    print("Result: ", result, file=output)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n1', '--number1', type=int, help='first number', default=1)
+    parser.add_argument('-n2', '--number2', type=int, help='second number', default=1)
+    parser.add_argument('-o', help='plik na dane wyjsciowe', default=sys.stdout, type=argparse.FileType('w'),
+                        dest='output')
+    parser.add_argument('-c', help='ścieżka do pliku konfiguracyjnego', type=argparse.FileType('r'))
+
+    args = parser.parse_args()
+    if args.c:
+        config = configparser.ConfigParser()
+        config.read_file(args.c)
+        args.number1 = int(config['ARGS']['n1'])
+        args.number2 = int(config['ARGS']['n2'])
+    main(args.number1, args.number2, args.output)

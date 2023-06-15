3bd95d1bab50840a03402c229fdb0de8758d2b78
dev-com2020 <kwp@tlen.pl>
2023-06-15 14:37:11 +0200

    15.06 14:37

---
+++ util/Duck.py
@@ -0,0 +1,54 @@
+class Duck:
+    def walk(self):
+        return "Duck walk"
+
+    def noise(self):
+        return "Kwa Kwa"
+
+
+class Dog:
+    def noise(self):
+        return "Hau Hau"
+
+    def march(self):
+        return "Dog march"
+
+
+class Cat(Duck, Dog):
+    pass
+
+
+class Cyclist:
+    def noise(self):
+        return "Ding Ding"
+
+    def pedal(self):
+        return "Cyclist pedal"
+
+
+class LoudMixin:
+    def noise(self):
+        return super().noise().upper()
+
+
+class AnnoyingMixin:
+    def noise(self):
+        return 3 * super().noise()
+
+class LoudDuck(LoudMixin, Duck):
+    pass
+
+class AnnoyingDog(AnnoyingMixin, Dog):
+    pass
+
+class LoudCyclist(LoudMixin, Cyclist):
+    pass
+
+d = LoudDuck()
+print(d.noise())
+g = AnnoyingDog()
+print(g.noise())
+c = LoudCyclist()
+print(c.noise())
+print(LoudCyclist.__mro__)
+print(AnnoyingDog.__mro__)
